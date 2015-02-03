7a4b86b07f563241963fa6547afe0f52bda5309f
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-03 12:20:48 +0530

    Version 1. implemented.

--- RPN.c
+++ RPN.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include "RPN.h"
 
+void printStack(Stack s){
+	Node* walker = s.top;
+	printf("-----------------------------Stack-----------------------\n");
+	printf("Top=%p\n",s.top );
+	printf("Count=%d\n",s.count );
+	while(walker != 0){
+		printf("Data = %d\n",*(int*)walker->data );
+		walker = walker -> next;
+	}
+}
+
 int calculate(int a, int b, char operator){
 	switch(operator){
 		case '+' : return b + a;
@@ -13,7 +24,7 @@ int calculate(int a, int b, char operator){
 	}
 }
 
-char* cat(char* string, char ch){
+char* concat(char* string, char ch){
 	int i,size = sizeof(char)*(strlen(string)+2);
 	char *new_string = malloc(size);
 	
@@ -26,7 +37,6 @@ char* cat(char* string, char ch){
 	}
 
 	new_string[i+1] = 0;
-	printf("new=%s\n",new_string );
 	return new_string;	
 }
 
@@ -36,26 +46,30 @@ Stack giveStackOfOperands(String expr){
 	char* num;
 	int length = strlen(expr);
 	Stack s = createStack();
+	num = calloc(1,sizeof(char)*10);
 
 	for (i=0 ; i<length ; i++){
 
-		if(isOperator(expr[i]))
+		if(isOperator(expr[i])){
+			if(num[0] != 0){
+				token = malloc(sizeof(int));
+				*token = atoi(num);
+				push(&s,token);
+				num = calloc(1,sizeof(char)*10);
+			}
 			break;
+		}
 
 		token = malloc(sizeof(int));
-		num = calloc(1,sizeof(char)*10);
-
+		
 		if(expr[i] != ' ' && expr[i] != 0){
-			printf("char=%c\n",expr[i] );
-			num = cat(num,expr[i]);
+			num = concat(num,expr[i]);
 			continue;
 		}
 		else{
-			printf("Number=%s\n",num );
 			*token = atoi(num);
 			push(&s,token);
-			num = calloc(1,sizeof(char)*5);
-			printf("%s\n",num );
+			num = calloc(1,sizeof(char)*10);
 		}		
 	}
 	return s;
@@ -71,7 +85,7 @@ int evaluate(String expr){
 	Stack operands = giveStackOfOperands(expr);
 	int a,b,*result;
 	int i,length = strlen(expr);
-	for(i=0 ; i<length && operands.count>0 ; i++){
+	for(i=0 ; i<length && operands.count>=2 ; i++){
 		if(!isOperator(expr[i]))
 			continue;
 		a = *(int*)pop(&operands);
